67bfb46b6dba231a4b7f8a8257ccf7534a45f8a3
Chris Bedwell <ckbedwell@gmail.com>
2022-02-10 09:11:51 +0000

    Fixed custom media queries for css

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,34 @@ module.exports = {
     'postcss-apply': {},
     'postcss-preset-env': {
       browsers: `last 2 versions`,
-      features: {
-        'custom-media-queries': {},
-      }
+      importFrom: [{
+        customMedia: generateMediaQueries({
+          "desktopSmall": 1200,
+          "tabletLarge": 1025,
+          "tabletMedium": 992,
+          "tabletSmall": 768,
+          "mobileLarge": 620
+        }),
+      }],
     },
   },
+}
+
+function generateMediaQueries(target) {
+  const prefixed = {}
+
+  for (let key in target) {
+    prefixed[`--above${capitalize(key)}`] = `(min-width: ${target[key] + 1}px)`
+    prefixed[`--${key}`] = `(max-width: ${target[key]}px)`
+  }
+
+  return prefixed
+}
+
+function capitalize(word = ``, strict) {
+  if (strict) {
+    return `${word.charAt(0).toUpperCase()}${word.slice(1).toLowerCase()}`
+  }
+
+  return `${word.charAt(0).toUpperCase()}${word.slice(1)}`
 }
